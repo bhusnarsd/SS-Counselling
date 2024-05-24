cddf61b1a12140577b4d8d35a7bfc51abae2af8c
suhas <suhas@simplifiedschooling.com>
2024-05-24 15:20:05 +0530

    changes

---
+++ src/models/reqLifeTrainer.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const requestLifeTainerSchema = new mongoose.Schema(
+  {
+    schoolId: {
+      type: String,
+      // ref: 'School',
+    },
+    visitDate: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+    },
+    schoolName: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+requestLifeTainerSchema.plugin(toJSON);
+requestLifeTainerSchema.plugin(paginate);
+
+const ReqLifeTrainer = mongoose.model('ReqLifeTrainer', requestLifeTainerSchema);
+module.exports = ReqLifeTrainer;
