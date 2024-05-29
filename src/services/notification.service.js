0889d8fd7e6e820ea00e7d85d88ab2ad479fec8a
suhas <suhas@simplifiedschooling.com>
2024-05-29 16:01:45 +0530

    chanegs roles

--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -10,8 +10,13 @@ const getNotificationsByUserId = async (userId) => {
   return Notification.find({ userId });
 };
 
-const markAsRead = async (notificationId) => {
-  return Notification.findByIdAndUpdate(notificationId, { read: true }, { new: true });
+const markAsRead = async (userId) => {
+  const result = await Notification.updateMany(
+    { userId }, // Find notifications by userId
+    { $set: { read: true } }, // Set the read field to true
+    { new: true } // Return the updated documents
+  );
+  return result;
 };
 
 module.exports = {
