967e4040e4c082c3e04ec23ecf5a0e9c451887bb
suhas <suhas@simplifiedschooling.com>
2024-05-29 11:42:57 +0530

    chanegs roles

--- src/routes/v1/visit.route.js
+++ src/routes/v1/visit.route.js
@@ -30,9 +30,7 @@ router
   .route('/get')
   .get(auth('admin', 'school', 'superadmin', 'student', 'trainer', 'department'), visitController.getTrainerVisits);
 
-  router
-  .route('/get/android')
-  .get(auth('admin', 'school', 'superadmin', 'student', 'trainer', 'department'), visitController.getTrainerVisits);
+router.route('/get/android').get(visitController.getTrainerVisits);
 
 router.route('/get-trainer-details/:schoolId').get(visitController.getVisitsBySchoolId);
 router.route('/update').patch(
